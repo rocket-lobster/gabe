4a7a7c185ad374ef204fe7b4ac265667551de3b5
Joe Thill <rocketlobster42@gmail.com>
2022-07-15 09:21:55 -0500

    Restructure joypad reading/writing

--- src/core/joypad.rs
+++ src/core/joypad.rs
@@ -1,62 +1,43 @@
+use super::gb::GbKeys;
 use super::interrupt::InterruptKind;
 use super::memory::Memory;
-use super::gb::GbKeys;
 
+/// The eight Game Boy action/direction buttons are arranged as a 2x4 matrix.
+/// Select either action or direction buttons by writing to this register, then read out the bits 0-3.
+/// Internally represents all 8 buttons as a single byte, then returns the correct nibble when read.
+/// Upper nibble: Action buttons
+/// Lower nibble: Directional buttons
 pub struct Joypad {
-    memory: u8,
+    state: u8,
+    using_directions: bool,
 }
 
 impl Joypad {
     pub fn power_on() -> Self {
-        Joypad {
-            memory: 0xFF,
-        }
+        Joypad { state: 0xFF, using_directions: false }
     }
 
     pub fn update(&mut self, keys_pressed: Option<&[GbKeys]>) -> Option<InterruptKind> {
-        let old_state = self.memory;
+        let old_state = self.state;
         if let Some(keys) = keys_pressed {
-            // Some keys were pressed
-            if !(self.memory | 0b1101_1111) != 0b0 {
-                // Action Select set
-                // Clear state of buttons
-                self.memory &= 0b1111;
-                keys.iter().for_each(|k| {
-                    match k {
-                        GbKeys::A => self.memory &= 0b1110,
-                        GbKeys::B => self.memory &= 0b1101,
-                        GbKeys::Select => self.memory &= 0b1011,
-                        GbKeys::Start => self.memory &= 0b0111,
-                        _ => ()
-                    }
-                });
-                // Check if any button was pressed only
-                if old_state & 0b1111 > self.memory & 0b1111 {
-                    Some(InterruptKind::Joypad)
-                } else {
-                    None
-                }
-            } else if !(self.memory | 0b1110_1111) != 0b0 {
-                // Direction Select set
-                // Clear state of buttons
-                self.memory &= 0b1111;
-                keys.iter().for_each(|k| {
-                    match k {
-                        GbKeys::Right => self.memory &= 0b1110,
-                        GbKeys::Left => self.memory &= 0b1101,
-                        GbKeys::Up => self.memory &= 0b1011,
-                        GbKeys::Down => self.memory &= 0b0111,
-                        _ => ()
-                    }
-                });
-                // Check if any button was pressed only
-                if old_state & 0b1111 > self.memory & 0b1111 {
-                    Some(InterruptKind::Joypad)
-                } else {
-                    None
-                }
+            // Reset values
+            self.state &= 0xFFu8;
+            keys.iter().for_each(|k| match k {
+                GbKeys::Start => self.state &= 0b0111_1111,
+                GbKeys::Select => self.state &= 0b1011_1111,
+                GbKeys::B => self.state &= 0b1101_1111,
+                GbKeys::A => self.state &= 0b1110_1111,
+                GbKeys::Down => self.state &= 0b1111_0111,
+                GbKeys::Up => self.state &= 0b1111_1011,
+                GbKeys::Left => self.state &= 0b1111_1101,
+                GbKeys::Right => self.state &= 0b1111_1110,
+            });
+            // Get which bits changed states
+            let cmp = old_state ^ self.state;
+            // AND with previous state, shows if any bits went high to low
+            if old_state & cmp != 0 {
+                Some(InterruptKind::Joypad)
             } else {
-                // Neither slot is selected, change nothing
                 None
             }
         } else {
@@ -69,23 +50,21 @@ impl Joypad {
 impl Memory for Joypad {
     fn read_byte(&self, addr: u16) -> u8 {
         assert!(addr == 0xFF00);
-        self.memory
+        if self.using_directions {
+            // Return directional pad values
+            (self.state | 0b1111_0000) & 0b1110_1111
+        } else {
+            // Return action pad values
+            ((self.state >> 4) | 0b1111_0000) & 0b1101_1111
+        }
     }
     fn write_byte(&mut self, addr: u16, val: u8) {
         assert!(addr == 0xFF00);
-        // Only write the upper nibble into the register, mask everything else off
+        // Only write the bit 4/5 into the register, mask everything else off
         match (val >> 4) & 0b11 {
-            0b00 => self.memory &= 0b1100_1111,
-            0b01 => {
-                self.memory &= 0b1101_1111;
-                self.memory |= 0b0001_0000;
-            }
-            0b10 => {
-                self.memory &= 0b1110_1111;
-                self.memory |= 0b0010_0000;
-            }
-            0b11 => self.memory |= 0b0011_0000,
-            _ => panic!("Logic error.")
-        } 
+            0b00 | 0b10 => self.using_directions = true,
+            0b01 | 0b11 => self.using_directions = false,
+            _ => panic!("Logic error."),
+        }
     }
 }
